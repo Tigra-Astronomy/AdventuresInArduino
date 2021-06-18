db8314b038d5730efa279bdb559f0a82454a9289
Tim Long <Tim@tigranetworks.co.uk>
2021-06-18 03:27:54 +0100

    Bonus content - external LEDs

--- CadencedIndicatorDemo/src/main.cpp
+++ CadencedIndicatorDemo/src/main.cpp
@@ -1,14 +1,24 @@
 #include <Arduino.h>
 #include <CadencedIndicator.h>
 
+namespace Cadence 
+{
+  constexpr CadencePattern SOS = 0b10101000111011101110001010100000ul;
+}
+
 CadenceManager ledManager;
 Indicator builtInLed(LED_BUILTIN, Cadence::Heartbeat | Cadence::Offbeat);
+Indicator externalBlueLed(2, Cadence::Wink);
+Indicator externalRedLed(3, Cadence::Blip);
+Indicator externalGreenLed(4, Cadence::SOS);
 
 void setup()
 {
   // put your setup code here, to run once:
-  pinMode(LED_BUILTIN, OUTPUT);
   ledManager.addIndicator(builtInLed);
+  ledManager.addIndicator(externalBlueLed);
+  ledManager.addIndicator(externalRedLed);
+  ledManager.addIndicator(externalGreenLed);
 }
 
 void loop()
